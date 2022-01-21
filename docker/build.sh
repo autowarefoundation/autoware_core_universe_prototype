dd5888bd76dc7b62891afa39a4b0b6c035781580
Kenji Miyake <kenji.miyake@tier4.jp>
2022-01-21 13:00:06 +0900

    fix: add --load to build.sh
    Signed-off-by: Kenji Miyake <kenji.miyake@tier4.jp>

--- docker/build.sh
+++ docker/build.sh
@@ -8,7 +8,7 @@ WORKSPACE_ROOT="$SCRIPT_DIR/../"
 # https://github.com/docker/buildx/issues/484
 export BUILDKIT_STEP_LOG_MAX_SIZE=10000000
 
-docker buildx bake --progress=plain -f "$SCRIPT_DIR/autoware-universe/docker-bake.hcl" \
+docker buildx bake --load --progress=plain -f "$SCRIPT_DIR/autoware-universe/docker-bake.hcl" \
     --set "*.context=$WORKSPACE_ROOT" \
     --set "*.ssh=default" \
     --set "devel.tags=ghcr.io/autowarefoundation/autoware-universe:latest" \
